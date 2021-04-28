dae60aa85815f97c02a547b2ddb71bfb6280b734
Mansi <mansi.chikarmane@gmail.com>
2021-04-28 10:36:15 -0700

    add lab file

---
+++ Lab1B_MansiChikarmane/Lab1B_MansiChikarmane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1B_MansiChikarmane
+//
+//  Created by Mansi Chikarmane on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
