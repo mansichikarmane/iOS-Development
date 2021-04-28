897ac6698ae88247b441690bd48dd460c8534d7a
Mansi <mansi.chikarmane@gmail.com>
2021-04-28 10:46:44 -0700

    add functions

--- Lab1B_MansiChikarmane/Lab1B_MansiChikarmane/ViewController.swift
+++ Lab1B_MansiChikarmane/Lab1B_MansiChikarmane/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // define two integer arrays, one for daily pay and one for total calculated pay
+    var dailyPay = [Int]()
+    var totalPay = [Int]()
+    
+    // define integer variable that stores the value for number of days of pay the pgm will calculate
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    
+    
 }
 
