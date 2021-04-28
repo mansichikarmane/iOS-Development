33958b63856b0651fb3a27b0e201ea837c85e205
Mansi <mansi.chikarmane@gmail.com>
2021-04-28 11:09:00 -0700

    add calculations

--- Lab1B_MansiChikarmane.playground/Contents.swift
+++ Lab1B_MansiChikarmane.playground/Contents.swift
@@ -1,21 +1,19 @@
 //: A UIKit based Playground for presenting user interface
   
 import UIKit
-import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
+// define two integer arrays, one for daily pay and one for total calculated pay
+var dailyPay = [Int]()
+var totalPay = [Int]()
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
-    }
+// define integer variable that stores the value for number of days of pay the pgm will calculate
+var numOfDays = [1, 2, 3, 4, 5]
+
+// pay per day. first day is 1 cent, second day is 2 cents, each day after it doubles
+var first = 0.01
+var second = 0.02
+
+while Int(second) < numOfDays.count {
+    Int(second) *= 2
+   print(dailyPay.append(Int(second)))
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
