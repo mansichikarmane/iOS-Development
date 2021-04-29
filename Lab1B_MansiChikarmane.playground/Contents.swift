f2e61508633038be129fbb6c402b9b6b18605c2d
Mansi <mansi.chikarmane@gmail.com>
2021-04-28 22:40:52 -0700

    add output

--- Lab1B_MansiChikarmane.playground/Contents.swift
+++ Lab1B_MansiChikarmane.playground/Contents.swift
@@ -3,40 +3,66 @@
 import UIKit
 
 // define two integer arrays, one for daily pay and one for total calculated pay
-var dailyPay:[Any] = [0.01]
-var totalPay:[Int] = []
+var dailyPay:[Any] = []
+var totalPay:[Any] = []
 
 // define integer variable that stores the value for number of days of pay the pgm will calculate
-var numOfDays = 31
-print("Days")
-for value in 1...31 {
-    print(value)
-}
+//var numOfDays = 30
+//print("Days")
+//for value in 1...numOfDays {
+//    print(value)
+//}
+
 // pay per day. first day is 1 cent, second day is 2 cents, each day after it doubles
 // calculates daily pay making double each day, adds to array called 'dailyPay'
 print("\nDaily Pay")
+var numOfDays = 30
 var first = 0.01
 var i = 0
+var tempValue = 0.0
 while (i < numOfDays) {
-    first *= 2
+    if (i == 0) {
+        dailyPay.append(contentsOf: [first])
+    }
+    else {
+        first *= 2
+        dailyPay.append(contentsOf: [first])
+    }
+    
+    tempValue += dailyPay[i] as! Double
+    tempValue = Double(round(100*tempValue)/100)
+    totalPay.append(contentsOf: [tempValue])
+    
     i += 1
-    dailyPay.append(contentsOf: [first])
 }
 print(dailyPay)
 
 // takes dailyPay and adds up each day
 print("\nTotal Pay")
-var k = 0
-var day1 = dailyPay[k]
-var day2 = dailyPay[k+1]
-func addDailys(day1: Int, day2: Int) {
-    while (k < dailyPay.count) {
-        k += 1
-        totalPay.append(contentsOf: [day1 + day2])
-    }
-    
-    print(totalPay)
+//i = 0
+//var tempValue = 0.0
+//
+//while (i < dailyPay.count) {
+//    tempValue += dailyPay[i] as! Double
+//    tempValue = Double(round(100*tempValue)/100)
+//    totalPay.append(contentsOf: [tempValue])
+//
+//    i += 1
+//}
+print(totalPay, "\n")
+
+
+// output results properly
+print("Day Daily Pay   Total Pay")
+i = 0
+while ( i < (numOfDays) ) {
+    i += 1
+    print(i, " $", dailyPay[i-1], " $", totalPay[i-1])
 }
+
+
+
+
 
 
 
