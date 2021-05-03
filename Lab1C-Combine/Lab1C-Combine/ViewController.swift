b174ab4d19c957200f9afeb04ff31308f8fa0be1
Mansi <mansi.chikarmane@gmail.com>
2021-05-03 12:12:57 -0700

    add comments and link from storyboard to view controller

--- Lab1C-Combine/Lab1C-Combine/ViewController.swift
+++ Lab1C-Combine/Lab1C-Combine/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // linking text inputs from storyboard
+    @IBOutlet var payInputField: UITextField!
+    @IBOutlet var dayInputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // styling of text inputs
+        payInputField.layer.cornerRadius = 10.0
+        payInputField.layer.masksToBounds = true
+        payInputField.layer.borderWidth = 0.5
+        
+        dayInputField.layer.cornerRadius = 10.0
+        dayInputField.layer.masksToBounds = true
+        dayInputField.layer.borderWidth = 0.5
+        
+        
+        
     }
 
 
